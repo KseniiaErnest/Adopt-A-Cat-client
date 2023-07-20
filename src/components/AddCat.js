3147d1a086fc14dde1f38e75ec1d65735545f7f2
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-07-20 18:33:04 -0400

    Working on AddCat.js

---
+++ src/components/AddCat.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const API_URL = "http://localhost:5005";
+
+export default function AddCat(props) {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(new Date());
+  const [breed, setBreed] = useState('');
+  const [gender, setGender] = useState('');
+  const [color, setColor] = useState('');
+  const [description, setDescription] = useState('');
+  const [availability, setAvailability] = useState('');
+  const [images, setImages] = useState([]);
+  const [dateOfEntry, setDateOfEntry] = useState(new Date());
+  const [location, setLocation] = useState([]);
+  // Fetching the location from API
+
+
+
+  return (
+    <div>
+      <h1>Add a cat for adoption</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label>Cat's name:</label>
+        <input type='text' name='name' value={name} onChange={(e) => setName(e.target.value)} />
+
+        <label>Cat's age:</label>
+        <input type='date' name='age' value={age} onChange={(e) => setAge(e.target.value)} />
+
+        <label>Cat's breed</label>
+        <input type='text' name='breed' value={breed} onChange={(e) => setBreed(e.target.value)} />
+
+        <label>Cat's gender:</label>
+        <input type='text' name='gender' value={gender} onChange={(e) => setGender(e.target.value)} />
+
+        <label>Cat's color:</label>
+        <input type='text' name='color' value={color} onChange={(e) => setColor(e.target.value)} />
+
+        <label>Cat's bio</label>
+        <textarea type='text' name='description' value={description} onChange={(e) => setDescription(e.target.value)} />
+
+        <label></label>
+      </form>
+    </div>
+  )
+}
