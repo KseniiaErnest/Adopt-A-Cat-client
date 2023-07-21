ebf6dada53982100edfcc1fb71b3810d20baebe6
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-07-20 21:48:47 -0400

    Working on upload images logic and component

--- src/components/AddCat.js
+++ src/components/AddCat.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const API_URL = "http://localhost:5005";
@@ -13,8 +13,18 @@ export default function AddCat(props) {
   const [availability, setAvailability] = useState('');
   const [images, setImages] = useState([]);
   const [dateOfEntry, setDateOfEntry] = useState(new Date());
-  const [location, setLocation] = useState([]);
+  const [locations, setLocations] = useState([]);
   // Fetching the location from API
+
+
+// Fetching location from the server
+useEffect(() => {
+  axios.get(`${API_URL}/locations`)
+  .then((response) => {
+    setLocations(response.data.allLocations);
+  })
+  .catch((err) => console.log(err));
+}, []);
 
 
 
@@ -33,7 +43,12 @@ export default function AddCat(props) {
         <input type='text' name='breed' value={breed} onChange={(e) => setBreed(e.target.value)} />
 
         <label>Cat's gender:</label>
-        <input type='text' name='gender' value={gender} onChange={(e) => setGender(e.target.value)} />
+       <select id='gender' value={gender} onChange={(e) => setGender(e.target.value)}>
+        <option value=''>Select gender</option>
+        <option value='Male'>Male</option>
+        <option value='Female'>Female</option>
+        <option value='Unknown'>Unknown</option>
+       </select>
 
         <label>Cat's color:</label>
         <input type='text' name='color' value={color} onChange={(e) => setColor(e.target.value)} />
@@ -41,7 +56,26 @@ export default function AddCat(props) {
         <label>Cat's bio</label>
         <textarea type='text' name='description' value={description} onChange={(e) => setDescription(e.target.value)} />
 
-        <label></label>
+        <label>Cat's status:</label>
+        <select id='availability' value={availability} onChange={(e) => setAvailability(e.target.value)}>
+        <option value='Available'>Available</option>
+        <option value='Adopted'>Adopted</option>
+        <option value='Pending'>Pending</option>
+        </select>
+
+        <label>Cat's images:</label>
+
+        <label>Cat's date of entry into the system</label>
+        <input type='date' name='dateOfEntry' value={dateOfEntry} onChange={(e) => setDateOfEntry(e.target.value)} />
+
+        <label>Cat is belong to:</label>
+        <select value={locations} onChange={(e) => setLocations(e.target.value)}>
+          <option value=''>Select a location</option>
+          {locations.map((location) => {
+            <option key={location._id} value={location._id}> {location.name} </option>
+          })}
+        </select>
+
       </form>
     </div>
   )
