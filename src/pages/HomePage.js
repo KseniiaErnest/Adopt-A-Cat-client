29fe9822c05f9e9f9598a9b640ff47695338e1f8
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-07-29 12:07:46 -0400

    API for cat fact

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function HomePage() {
+  const [catFact, setCatFact] = useState('');
+
+  const getCatFact = () => {
+    axios.get(`https://meowfacts.herokuapp.com/`)
+    .then((response) => {
+      setCatFact(response.data.data[0])
+    })
+    .catch((err) => console.log(err));
+  };
+
+
   return (
     <div className='home-grid'>
       <div className='home-container-one'>
 <p>jfjfjkjgkvjckfkdgjfdlgjfdklgfldgjlfdblfkdmlkfdgjkfldgjlfdgldfgjlfdgjl</p>
 <button>Sign Up</button>
+<h2>Here is some fact about cats!</h2>
+<button onClick={getCatFact}>Get a fact</button>
+<p>{catFact}</p>
 
       </div>
 
