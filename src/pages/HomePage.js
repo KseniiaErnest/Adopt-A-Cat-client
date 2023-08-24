2953137ba860ccb59f6a910a2eb16fd8079227ea
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-08-24 11:42:06 -0400

    Updated text

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -27,12 +27,12 @@ export default function HomePage() {
     }}>
 
     <div className='home-text-box'>
-    <h1>Welcome to the Adopt a Cat App! </h1>
+    <h1>Welcome to the Adopt a Pet App! </h1>
     <p className='home-text'>
-Why choose cats, you might ask? Well, who can resist the charm of these adorable, fluffy creatures with a knack for occasional mischief? Cats have an uncanny ability to shower us with love and provide endless comfort, and they bring their own unique brand of "cattitude" to our lives.
+    Why choose between cats and dogs Who can resist the charm of these adorable, fluffy creatures with a knack for occasional mischief and adventurous spirit? Both cats and dogs have an uncanny ability to shower us with love and provide endless comfort, and they bring their own unique traits to our lives.
 
 </p>
-<p className='home-text'>At Adopt a Cat, our mission is to bridge the gap between rescue organizations and loving forever homes. We're here to unite future fur moms and dads with their perfect feline companions. Hold onto your catnip, because you're about to embark on a journey that's part heartwarming, part hilarious, and all things whiskers!</p>
+<p className='home-text'>At Adopt a Pet, our mission is to bridge the gap between rescue organizations and loving forever homes, whether you're looking for a purrfect feline friend or a loyal canine companion. We're here to unite future pet parents with their ideal furry companions. Hold onto your treats and toys, because you're about to embark on a journey that's part heartwarming, part hilarious, and all things paws and whiskers!</p>
 <h2>Click on the paw for some facts about cats!</h2>
  <button className=' btn--get-fact' onClick={getCatFact}><img src='/icons8-cat-footprint-50.png' alt='cat paw' /></button>
  <p className='api-fact'>{catFact}</p>
