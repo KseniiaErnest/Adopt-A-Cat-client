258f24b1ec58cd0ec2cda2ee593e2df70eb845ba
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-08-24 21:47:28 -0400

    Updated the route connection

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { AuthContext } from '../context/auth.context';
 
-const API_URL = "http://localhost:5005";
+// const API_URL = "http://localhost:5005";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5005";
 
 export default function UserProfile( {closeModal} ) {
   const { user } = useContext(AuthContext);
